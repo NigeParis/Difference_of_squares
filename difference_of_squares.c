57d05b8d69d50b1c8957573fb11bd228810bceca
42_Nige <42_Legin@MacBook-Pro-de-Nige-Paris.local>
2023-06-16 12:08:46 +0200

    modification mise en page

--- difference_of_squares.c
+++ difference_of_squares.c
@@ -6,7 +6,7 @@
 /*   By: 42_Legin <Nige@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 19:36:55 by 42_Legin          #+#    #+#             */
-/*   Updated: 2023/06/16 11:08:16 by 42_Legin         ###   ########.fr       */
+/*   Updated: 2023/06/16 12:08:09 by 42_Legin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,8 @@ unsigned int	square_of_sum(unsigned int number)
 	result = 1;
 	while (number > 1)
 	{
-			result += number;
-			number--;
+		result += number;
+		number--;
 	}
 	result *= result;
 	return (result);
@@ -56,9 +56,9 @@ unsigned int	sum_of_squares(unsigned int number)
 	square = 0;
 	while (number > 1)
 	{
-			square = number * number;
-			result += square;
-			number--;
+		square = number * number;
+		result += square;
+		number--;
 	}
 	return (result);
 }
